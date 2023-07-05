2f92c09c703c41b8629fcf7acae51631fae558f6
sunznx <sunznx@163.com>
2023-07-05 16:27:26 +0800

    feat: setup `exec-path-from-shell`

--- .custom.el
+++ .custom.el
@@ -2,3 +2,7 @@
 (setq keyfreq-file (concat my-emacs-d ".keyfreq"))
 (setq keyfreq-file-lock (concat my-emacs-d ".keyfreq.lock"))
 (turnon-keyfreq-mode)
+
+
+;;; exec-path-from-shell
+(setq exec-path-from-shell-check-startup-files nil)
